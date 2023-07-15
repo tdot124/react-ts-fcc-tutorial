9efe5fc39b43a8db669c4afe20589546cf929562
Tom Mills <t.mills124@gmail.com>
2023-07-15 17:21:20 +0100

    added todos state and ability to add to todos array if input is submitted and not blank

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -5,6 +5,7 @@ import { Input } from './Input'
 
 export const AddTodo = () => {
   const [input, setInput] = useState<string>('')
+  const [todos, setTodos] = useState<string[]>([])
   const inputRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
@@ -15,7 +16,10 @@ export const AddTodo = () => {
 
   const handleSubmission = (e: React.FormEvent) => {
     e.preventDefault()
-    console.log('form has been submitted')
+    if (input.trim() !== '') {
+      setTodos([...todos, input])
+      setInput('')
+    }
   }
 
   return (
